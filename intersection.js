9b8ca6419e7fa6e7ae586943d1ec4abaceb792f9
Amy M Haddad <amy.m.haddad@gmail.com>
2020-01-08 08:50:50 -0500

    Adding js files

--- intersection.js
+++ intersection.js
@@ -1,6 +1,12 @@
 
-let allNumbers = [[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]];
-for (let i=0; i<allNumbers.length; i++) {
+const intersection = (...arrays) => {
+    let allNumbers = [...arrays];
+    let i = 0;
     let intersect = new Set([...allNumbers[i]].filter(number => new Set(allNumbers[i+1]).has(number)));
-    console.log(intersect);
-}   
+    i += 1;
+    const total = [...intersect];
+    return total;
+
+};
+
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
